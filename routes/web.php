2bad322e0335f1d46c032f67cb415768d9e33cf3
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-01-31 16:26:22 +0500

    Admin blade qo'shildi

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// })->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+})->name('dashboard');
 
 Route::prefix('hr')->name('hr.')->group(function () {
     Route::resource('role', RoleController::class);
