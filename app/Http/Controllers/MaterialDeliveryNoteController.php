fb0bedee861d0cfe83a6122731fa358b2a3f77a9
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-02-06 21:30:05 +0500

    galdi getdi boshlandi

---
+++ app/Http/Controllers/MaterialDeliveryNoteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MaterialDeliveryNote;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MaterialDeliveryNoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MaterialDeliveryNote $materialDeliveryNote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MaterialDeliveryNote $materialDeliveryNote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MaterialDeliveryNote $materialDeliveryNote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MaterialDeliveryNote $materialDeliveryNote)
+    {
+        //
+    }
+}
