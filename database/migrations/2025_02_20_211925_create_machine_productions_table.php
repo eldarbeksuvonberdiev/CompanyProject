aa965ae47163080dc1f6eeab5aacf44c63b8af04
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2025-02-20 21:50:55 +0500

    Livewire qo'shildi oxiri

---
+++ database/migrations/2025_02_20_211925_create_machine_productions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('machine_productions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('production_id')->constrained('productions')->cascadeOnDelete();
+            $table->foreignId('machine_id')->constrained('machines')->cascadeOnDelete();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('count');
+            $table->integer('defected')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('machine_productions');
+    }
+};
